622923549fc9071c0224f897f4131341c1835ed7
hitender <hpsingh3330@gmail.com>
2017-05-27 20:37:49 +0530

    code updated

--- test1.sh
+++ test1.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-hi 
+#turn on globing
+shopt -s extglob
 
+path="/root/test"
+save="/root/save"
+
+ls /root/test/ |egrep "30|31" > $save
+find /root/test/*txt -type f -exec rm -rf !(*30*|*31*) \;
+
+#turn off globing
+shopt -u extglob
